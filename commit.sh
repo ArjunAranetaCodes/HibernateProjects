bd407dbf95dae0cad8d365b21df367f59e5d6895
MoreCodes <arjunaraneta@gmail.com>
2023-09-30 16:19:00 +0800

    Hibernate Projects Changes

---
+++ commit.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+git add .
+git commit --date "2023-09-30" -m "Hibernate Projects Changes"
+git push origin
--- todo-application-1-frontend/todo-frontend/src/components/Todo.js
+++ todo-application-1-frontend/todo-frontend/src/components/Todo.js
@@ -4,16 +4,18 @@ import React from 'react';
 const Todo = ({ todo, onEdit, onDelete }) => {
     return (
         <div className="card text-dark bg-light mb-3">
-            <div className="card-body">
-                <h5 className="card-title">{todo.title}</h5>
-                <p className="card-text">{todo.description}</p>
+            <div className='row'>
+                <div className="card-body">
+                    <h5 className="card-title">{todo.title}</h5>
+                    <p className="card-text">{todo.description}</p>
+                </div>
+                <button className="btn btn-primary btn-sm ms-2" onClick={() => onEdit(todo.id)}>
+                    Edit
+                </button>
+                <button className="btn btn-danger btn-sm ms-2" onClick={() => onDelete(todo.id)}>
+                    Delete
+                </button>
             </div>
-            <button className="btn btn-primary btn-sm ms-2" onClick={() => onEdit(todo.id)}>
-                Edit
-            </button>
-            <button className="btn btn-danger btn-sm ms-2" onClick={() => onDelete(todo.id)}>
-                Delete
-            </button>
         </div>
     );
 };
