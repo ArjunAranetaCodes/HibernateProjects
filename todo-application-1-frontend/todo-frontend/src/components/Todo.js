14874d740969b516f73f00206101b6cb9fd02f02
MoreCodes <arjunaraneta@gmail.com>
2023-09-30 16:20:24 +0800

    Hibernate Projects Changes

--- todo-application-1-frontend/todo-frontend/src/components/Todo.js
+++ todo-application-1-frontend/todo-frontend/src/components/Todo.js
@@ -5,16 +5,20 @@ const Todo = ({ todo, onEdit, onDelete }) => {
     return (
         <div className="card text-dark bg-light mb-3">
             <div className='row'>
-                <div className="card-body">
-                    <h5 className="card-title">{todo.title}</h5>
-                    <p className="card-text">{todo.description}</p>
+                <div className='col-8'>
+                    <div className="card-body">
+                        <h5 className="card-title">{todo.title}</h5>
+                        <p className="card-text">{todo.description}</p>
+                    </div>
                 </div>
-                <button className="btn btn-primary btn-sm ms-2" onClick={() => onEdit(todo.id)}>
-                    Edit
-                </button>
-                <button className="btn btn-danger btn-sm ms-2" onClick={() => onDelete(todo.id)}>
-                    Delete
-                </button>
+                <div className='col-2'>
+                    <button className="btn btn-primary btn-sm ms-2" onClick={() => onEdit(todo.id)}>
+                        Edit
+                    </button>
+                    <button className="btn btn-danger btn-sm ms-2" onClick={() => onDelete(todo.id)}>
+                        Delete
+                    </button>
+                </div>
             </div>
         </div>
     );
