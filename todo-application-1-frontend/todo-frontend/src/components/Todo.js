a674f4093b7b76c12a1d80eea576eea19a97d39b
MoreCodes <arjunaraneta@gmail.com>
2024-01-11 20:30:59 +0800

    Update Todo.js

--- todo-application-1-frontend/todo-frontend/src/components/Todo.js
+++ todo-application-1-frontend/todo-frontend/src/components/Todo.js
@@ -5,7 +5,7 @@ const Todo = ({ todo, onEdit, onDelete }) => {
     return (
         <div className="mb-3">
             <span>{todo.title}</span>
-            <button className="btn btn-danger btn-sm ms-2" onClick={() => onEdit(todo.id)}>
+            <button className="btn btn-primary btn-sm ms-2" onClick={() => onEdit(todo.id)}>
                 Edit
             </button>
             <button className="btn btn-danger btn-sm ms-2" onClick={() => onDelete(todo.id)}>
