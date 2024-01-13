23d7f483f4e6cff5f365e9883fe13d460886d03c
MoreCodes <arjunaraneta@gmail.com>
2024-01-13 16:13:04 +0800

    Update Todo.js

--- todo-application-1-frontend/todo-frontend/src/components/Todo.js
+++ todo-application-1-frontend/todo-frontend/src/components/Todo.js
@@ -3,8 +3,11 @@ import React from 'react';
 
 const Todo = ({ todo, onEdit, onDelete }) => {
     return (
-        <div className="mb-3">
-            <span>{todo.title}</span>
+        <div className="card text-dark bg-light mb-3">
+            <div className="card-body">
+                <h5 className="card-title">{todo.title}</h5>
+                <p className="card-text">{todo.description}</p>
+            </div>
             <button className="btn btn-primary btn-sm ms-2" onClick={() => onEdit(todo.id)}>
                 Edit
             </button>
